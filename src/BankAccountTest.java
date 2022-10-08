70a79e7ff0c405b9ab620bef95ed160327b138e4
Amir Sediqi <amir.sediqi69@gmail.com>
2022-10-08 17:37:32 +0200

    Succesful Test Method

--- src/BankAccountTest.java
+++ src/BankAccountTest.java
@@ -4,7 +4,9 @@ public class BankAccountTest {
 
     @org.junit.Test
     public void deposit() {
-        fail("This test has yet to be implemented");
+        BankAccount account = new BankAccount("Amir", "Sediqi", 100);
+        double balans = account.deposit(100, true);
+        assertEquals(200, 200, 0);
     }
 
     @org.junit.Test
@@ -15,5 +17,10 @@ public class BankAccountTest {
     @org.junit.Test
     public void getBalance() {
         fail("This test has yet to be implemented");
+    }
+
+    @org.junit.Test
+    public void dummyTest(){
+        assertEquals(1, 1);
     }
 }
